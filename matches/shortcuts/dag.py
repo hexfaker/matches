1e62c29b03aa8eb04bad6ad2fb0ebef12dcb63e7
Vsevolod Poletaev <poletaev.va@gmail.com>
2022-06-17 20:24:51 +0400

    ComputationGraph enhancements

--- matches/shortcuts/dag.py
+++ matches/shortcuts/dag.py
@@ -1,18 +1,28 @@
+import functools
+import logging
 from contextlib import contextmanager
 from inspect import signature
-from typing import Callable, Optional, Sequence, TypeVar
+from typing import Callable, TypeVar
 
 import torch
+from torch.autograd.profiler import record_function
+from typing_extensions import ParamSpec
 
-_T = TypeVar("_T")
+R = TypeVar("R")
+Args = ParamSpec("Args")
 
 
-def graph_node(method: Callable[..., _T], *, cache_key_args: Optional[Sequence[str]] = None) -> Callable[..., _T]:
+def graph_node(
+    method: Callable[Args, R],
+) -> Callable[Args, R]:
     sig = signature(method)
     if "self" not in sig.parameters:
-        raise ValueError("graph_node can only be used on ComputationGraph subclass methods")
+        raise ValueError(
+            "graph_node can only be used on ComputationGraph subclass methods"
+        )
 
-    def _wrapper(self: ComputationGraph, **kwargs):
+    @functools.wraps(method)
+    def _wrapper(self, **kwargs):
         return self.get_or_compute_node(method, **kwargs)
 
     return _wrapper
@@ -38,17 +48,22 @@ class ComputationGraph:
             raise ValueError("Attempt to use cache outside cache_scope()")
         return self._cache.get(key, None)
 
-    def get_or_compute_node(self, method: Callable[..., _T], **kwargs):
-        key = method.__name__
+    def get_or_compute_node(self, method, **kwargs):
+        args = tuple([(k, kwargs[k]) for k in sorted(kwargs.keys())])
+        name = method.__name__
+        key = (name, args)
         value = self.get_cache_entry(key)
 
         if value is not None:
+            logging.debug("Node <%s> cache hit", key)
             if torch.is_tensor(value) and not torch.is_grad_enabled():
                 # Ensure detached version is returned if gradient is disabled globally
                 value = value.detach()
             return value
 
-        value = method(self, **kwargs)
+        logging.debug("Node <%s> compute", key)
+        with record_function(name):
+            value = method(self, **kwargs)
         self._cache[key] = value
 
         return value
